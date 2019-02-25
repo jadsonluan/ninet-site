55bbea18a4775a882cf0ff9cee73bbc43b088c83
Douglas Lima <douglas.lima@ccc.ufcg.edu.br>
2019-02-24 21:04:07 -0300

    Fix transição de fotos

--- v1/experiencia/index.js
+++ v1/experiencia/index.js
@@ -11,15 +11,25 @@ function expand(i, btn) {
     }
 }
 
-var slideIndex = [1,1, 1, 1, 1, ];
+var slideIndex = [1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 /* Class the members of each slideshow group with different CSS classes */
-var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6"]
+var slideId = ["mySlides1", "mySlides2", "mySlide3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13", "mySlides14", "mySlides15", "mySlides16"]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
 showSlides(1, 3);
 showSlides(1, 4);
 showSlides(1, 5);
+showSlides(1, 6);
+showSlides(1, 7);
+showSlides(1, 8);
+showSlides(1, 9);
+showSlides(1, 10);
+showSlides(1, 11);
+showSlides(1, 12);
+showSlides(1, 13);
+showSlides(1, 14);
+showSlides(1, 15);
 
 // Thumbnail image controls
 function currentSlide(n) {
