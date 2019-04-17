d35667b7e264080faa62e9fbdc22e748f4951fc0
Douglas Lima <douglas.lima@ccc.ufcg.edu.br>
2019-04-16 22:45:02 -0300

    Correção de erro em seção de Experiência

--- v1/experiencia/index.js
+++ v1/experiencia/index.js
@@ -27,9 +27,9 @@ function expand(i, btn) {
     }
 }
 
-var slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+var slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1, 1];
 /* Class the members of each slideshow group with different CSS classes */
-var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13", "mySlides14", "mySlides15", "mySlides16"]
+var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13", "mySlides14", "mySlides15", "mySlides16", "mySlides17"]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
@@ -46,6 +46,7 @@ showSlides(1, 12);
 showSlides(1, 13);
 showSlides(1, 14);
 showSlides(1, 15);
+showSlides(1, 16);
 
 // Thumbnail image controls
 function currentSlide(n) {
